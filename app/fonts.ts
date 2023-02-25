2aec015327065e20b7ae0a3ded5c994fd44af005
Elias <eeschmoelz@gmail.com>
2023-02-25 12:02:32 +0000

    feat: preload inter variable font

---
+++ app/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter } from 'next/font/google';
+
+export const InterFont = Inter({
+    subsets: ['latin'],
+    display: 'swap',
+    variable: '--Inter-Font',
+    adjustFontFallback: true
+})
