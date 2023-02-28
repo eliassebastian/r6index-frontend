d25288c28d7323ee828d65af78d0dacb2bc4f855
Elias <eeschmoelz@gmail.com>
2023-02-28 19:10:27 +0000

    fix: ts(2339) error

--- context/SearchDialogContext/index.tsx
+++ context/SearchDialogContext/index.tsx
@@ -1,5 +1,5 @@
 import { createContext, Dispatch, SetStateAction } from "react";
 
-const SearchDialogContext = createContext<{ isVisible: boolean; setVisible: Dispatch<SetStateAction<boolean>>} | null>(null);
+const SearchDialogContext = createContext<{ isVisible: boolean; setVisible: Dispatch<SetStateAction<boolean>>}>( { isVisible: false, setVisible: () => {}} );
 
 export default SearchDialogContext;
