e995f8a572af37d37c8de09a10c1613fe3eece12
Elias <eeschmoelz@gmail.com>
2023-02-28 12:04:56 +0000

    feat: search dialog context provider

---
+++ components/Search/SearchDialogWrapper/index.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import SearchDialogContext from "@/context/SearchDialogContext";
+import { cloneElement, useState, type ReactElement } from "react";
+import SearchDialog from "../SearchDialog";
+
+interface SearchDialogProps {
+    children: ReactElement;
+}
+
+const SearchDialogWrapper = ({ children }: SearchDialogProps) => {
+
+    const [isVisible, setVisible] = useState(false);
+    const clonedElement = cloneElement(children, { onclick: () => { console.log('click'); setVisible(value => !value) } });
+
+    return (
+        <>
+            { clonedElement }
+            <SearchDialogContext.Provider value={{ isVisible, setVisible }}>
+                <SearchDialog/>
+            </SearchDialogContext.Provider>
+        </>
+    )
+};
+
+
+export default SearchDialogWrapper;
