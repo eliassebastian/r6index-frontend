0e21b8cbc6e4a6b98d768bb4cc33c72fdc1581a9
Elias <eeschmoelz@gmail.com>
2023-02-28 22:23:15 +0000

    feat: search dialog modal component

--- components/Search/SearchDialog/index.tsx
+++ components/Search/SearchDialog/index.tsx
@@ -1,17 +1,33 @@
 'use client';
 
 import SearchDialogContext from '@/context/SearchDialogContext';
-import { Dispatch, SetStateAction, useContext } from 'react';
+import { useContext, useLayoutEffect } from 'react';
+import SearchBar from '../SearchBar';
 import styles from './SearchDialog.module.scss';
 
 const SearchDialog = () => {
     const { isVisible, setVisible } = useContext(SearchDialogContext);
 
+    useLayoutEffect(() => {
+        if (!isVisible) return;
+    }, [isVisible]);
+
     if (!isVisible) return null;
 
     return (
         <div className={styles.wrapper}>
-            
+            <div className={styles.background}>
+                <div className={styles.backdrop} onPointerDown={() => { setVisible(false) }}></div>
+                <div className={styles.container}>
+                    <div className={styles.padding}>
+                        <div className={styles.content}>
+                            <div className={styles.searchbar_wrapper}>
+                                <SearchBar/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 };
