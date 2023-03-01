881035a83ac09bdfadc4813087dac9b3952bf247
Elias <eeschmoelz@gmail.com>
2023-03-01 00:05:46 +0000

    fix: search bar dialog close through prop

--- components/Search/SearchBar/index.tsx
+++ components/Search/SearchBar/index.tsx
@@ -1,9 +1,13 @@
 'use client';
 
-import { useRef, useState } from "react";
+import { useCallback, useRef, useState } from "react";
 import styles from './SearchBar.module.scss';
 
-const SearchBar = () => {
+interface SearchBarProps {
+    onClose: () => void;
+}
+
+const SearchBar = ({ onClose }: SearchBarProps) => {
     const inputRef = useRef<HTMLInputElement>(null);
     const [search, setSearch] = useState('');
 
@@ -17,9 +21,14 @@ const SearchBar = () => {
         console.log(search);
     }
 
+    const closeDialog = useCallback((e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        onClose();
+    }, []);
+
     return (
         <form className={styles.form} onSubmit={onSubmit}>
-            <button className={styles.back_btn}>
+            <button className={styles.back_btn} onClick={closeDialog}>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                     <path fillRule="evenodd" d="M17 10a.75.75 0 01-.75.75H5.612l4.158 3.96a.75.75 0 11-1.04 1.08l-5.5-5.25a.75.75 0 010-1.08l5.5-5.25a.75.75 0 111.04 1.08L5.612 9.25H16.25A.75.75 0 0117 10z" clipRule="evenodd" />
                 </svg>
