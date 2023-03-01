8081b35cafef0b2d715bf9f966fe5f92920271a4
Elias <eeschmoelz@gmail.com>
2023-03-01 11:08:32 +0000

    feat: persist platform state management

---
+++ stores/PlatformStore/index.tsx
@@ -0,0 +1,28 @@
+import { create } from 'zustand'
+import { persist, createJSONStorage } from 'zustand/middleware'
+
+type Platform = 0 | 1 | 2;
+
+type State = {
+    platform: Platform;
+    setPlatform: (platform: Platform) => void
+}
+
+enum PlatformEnum {
+    PC = 0,
+    PS = 1,
+    XBOX = 2
+}
+
+export const usePlatformStore = create<State, [["zustand/persist", State]]>(
+    persist(
+        (set) => ({
+            platform: 0,
+            setPlatform: (platform: Platform) => set({ platform: platform }),
+        }),
+        {
+            name: 'r6index-platform',
+            storage: createJSONStorage(() => localStorage)
+        }
+    )
+)
