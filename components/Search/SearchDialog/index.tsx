24d99c237234859a0cab08447e337095ae24fc63
Elias <eeschmoelz@gmail.com>
2023-03-01 00:05:30 +0000

    fix: close dialog function prop

--- components/Search/SearchDialog/index.tsx
+++ components/Search/SearchDialog/index.tsx
@@ -8,6 +8,10 @@ import styles from './SearchDialog.module.scss';
 const SearchDialog = () => {
     const { isVisible, setVisible } = useContext(SearchDialogContext);
 
+    const closeDialog = () => {
+        setVisible(false);
+    }
+
     useLayoutEffect(() => {
         if (!isVisible) return;
     }, [isVisible]);
@@ -17,12 +21,12 @@ const SearchDialog = () => {
     return (
         <div className={styles.wrapper}>
             <div className={styles.background}>
-                <div className={styles.backdrop} onPointerDown={() => { setVisible(false) }}></div>
+                <div className={styles.backdrop} onPointerDown={closeDialog}></div>
                 <div className={styles.container}>
                     <div className={styles.padding}>
                         <div className={styles.content}>
                             <div className={styles.searchbar_wrapper}>
-                                <SearchBar/>
+                                <SearchBar onClose={closeDialog}/>
                             </div>
                         </div>
                     </div>
