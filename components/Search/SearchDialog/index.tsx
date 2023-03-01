5a6fec59d35e24a4dc26a8d54c210b8c3694ffea
Elias <eeschmoelz@gmail.com>
2023-03-01 00:58:44 +0000

    feat: search dialog close btn + overflow body

--- components/Search/SearchDialog/index.tsx
+++ components/Search/SearchDialog/index.tsx
@@ -14,6 +14,14 @@ const SearchDialog = () => {
 
     useLayoutEffect(() => {
         if (!isVisible) return;
+
+        document.body.style.overflow = 'hidden';
+        document.body.style.position = 'fixed';
+
+        return () => {
+            document.body.style.overflow = '';
+            document.body.style.position = '';
+        }
     }, [isVisible]);
 
     if (!isVisible) return null;
@@ -30,6 +38,11 @@ const SearchDialog = () => {
                             </div>
                         </div>
                     </div>
+                    <button className={styles.close_btn} onClick={closeDialog}>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                            <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
+                        </svg>
+                    </button>
                 </div>
             </div>
         </div>
