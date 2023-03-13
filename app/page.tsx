7f49417831150495dbc1de4a750e32f2b0cdeb8c
Elias <eeschmoelz@gmail.com>
2023-03-13 14:31:09 +0000

    fix: use homebackground component

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,15 @@
-import MotionBorderButton from '@/components/Buttons/MotionBorderButton'
 import FooterHome from '@/components/Footer/FooterHome'
 import HomeAction from '@/components/Home/HomeAction'
-import Logo from '@/components/Logo'
 import styles from './page.module.scss'
+
+import HomeBackground from '@/components/Backgrounds/BackgroundHome'
 
 export default function Home() {
   return (
     <>
       <main className={styles.main}>
-        <div className={styles.logo_wrapper}>
-          <Logo/>
-        </div>
-        <MotionBorderButton><h1>Test</h1></MotionBorderButton>
+        <HomeBackground/>
+          {/* <div className={styles.gradient}></div> */}
         <HomeAction/>
       </main>
       <FooterHome/>
