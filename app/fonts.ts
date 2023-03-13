6e6467063baa6b7f376c1409a913606b456cc34c
Elias <eeschmoelz@gmail.com>
2023-03-13 14:30:05 +0000

    fix: font css variable naming

--- app/fonts.ts
+++ app/fonts.ts
@@ -3,6 +3,6 @@ import { Inter } from 'next/font/google';
 export const InterFont = Inter({
     subsets: ['latin'],
     display: 'swap',
-    variable: '--Inter-Font',
+    variable: '--font-inter',
     adjustFontFallback: true
 })
