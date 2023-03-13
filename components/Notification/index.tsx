46ca8cddf8ede08f6c5afeea887c0ed770ff8071
Elias <eeschmoelz@gmail.com>
2023-03-13 14:29:35 +0000

    feat: notification component

---
+++ components/Notification/index.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import { useState } from 'react';
+import styles from './Notification.module.scss';
+
+interface NotificationProps {
+    children: React.ReactNode;
+    variant: 'success' | 'error' | 'warning' | 'info';
+}
+
+const Notification = ({ children, variant }: NotificationProps) => {
+    const [hide, setHide] = useState(true);
+
+    if (!hide) return null;
+
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.symbol}>
+                {
+                    variant === 'success' &&
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                    </svg>
+                }
+                {
+                    variant === 'error' &&
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636" />
+                    </svg>
+                }
+                {
+                    variant === 'warning' &&
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
+                    </svg>
+                }
+                {
+                    variant === 'info' &&
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
+                    </svg>
+                }
+            </div>
+            <div className={styles.content}>
+                {children}
+            </div>
+            <div className={styles.btn}>
+                <button className={styles.close_btn}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                        <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
+                    </svg>
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Notification;
