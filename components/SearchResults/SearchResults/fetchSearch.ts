8792f9a400604b956816cecd8307017c14c64dce
Elias <eeschmoelz@gmail.com>
2023-03-15 18:37:39 +0000

    feat: fetch search api call

--- components/SearchResults/SearchResults/fetchSearch.ts
+++ components/SearchResults/SearchResults/fetchSearch.ts
@@ -1,14 +1,14 @@
+import type { SearchResult } from "@/types/SearchResults";
 
 const getSearch = async (user: string, platform: string) => {
-    const res = await fetch(`https://37578276-6c45-4460-b201-a598226c9cf5.mock.pstmn.io/v1/search?p=${platform}&q=${user}`, { cache: 'no-store' });
-
-    //throw new Error('Failed to fetch data');
+    const res = await fetch(`http://127.0.0.1:8080/v1/search?p=${platform}&q=${user}`, { cache: 'no-store' });
 
     if (res.status !== 200) {
         throw new Error('Failed to fetch data');
     }
 
-    return res.json();
+    const results: SearchResult = await res.json();
+    return results;
 }
 
 export default getSearch;
