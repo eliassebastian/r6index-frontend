de0b1ccaf966c7d992b708f0d8dfb532c81a3b5e
Elias <eeschmoelz@gmail.com>
2023-03-15 18:40:10 +0000

    feat: search results trending list component

---
+++ components/SearchResults/Trending/TrendingList/index.tsx
@@ -0,0 +1,21 @@
+import TrendingProfile from '../TrendingProfile';
+import styles from './TrendingList.module.scss';
+
+const TrendingList = () => {        
+    return (
+        <ul className={styles.list}>
+            <TrendingProfile name="spoit.koi" id="ab1ff7ae-13e4-4a6a-9b03-317285f8057b" />
+            <TrendingProfile name="leongids.koi" id="661e632b-c2cd-4994-96ba-6d9ed930b941" />
+            <TrendingProfile name="pengu.dz" id="f1b010e1-d441-47b1-be1f-cea6e2f22e19" />
+            <TrendingProfile name="rath.-" id="eac01e72-a88c-4847-ad73-0e03ce7a1870" />
+            <TrendingProfile name="EddyDTB" id="95171ef3-32d7-466f-a8ea-438b845444e3" />
+            <TrendingProfile name="EddyDTB" id="95171ef3-32d7-466f-a8ea-438b845444e3" />
+            <TrendingProfile name="EddyDTB" id="95171ef3-32d7-466f-a8ea-438b845444e3" />
+            <TrendingProfile name="EddyDTB" id="95171ef3-32d7-466f-a8ea-438b845444e3" />
+            <TrendingProfile name="EddyDTB" id="95171ef3-32d7-466f-a8ea-438b845444e3" />
+            <TrendingProfile name="EddyDTB" id="95171ef3-32d7-466f-a8ea-438b845444e3" />
+        </ul>
+    );
+};
+
+export default TrendingList;
