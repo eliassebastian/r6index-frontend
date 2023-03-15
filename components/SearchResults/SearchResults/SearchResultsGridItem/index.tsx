0f8117d0a93f8a64a3c3c372b1aa843983219eb1
Elias <eeschmoelz@gmail.com>
2023-03-15 19:38:31 +0000

    feat: highlight exact search and previous alias

--- components/SearchResults/SearchResults/SearchResultsGridItem/index.tsx
+++ components/SearchResults/SearchResults/SearchResultsGridItem/index.tsx
@@ -6,19 +6,29 @@ import { timeAgo } from '@/utils/Time';
 import Link from 'next/link';
 import styles from './SearchResultsGridItem.module.scss';
 
-const SearchResultsGridItem = (props: Hit) => {
+const SearchResultsGridItem = (props: { info: Hit, query: string }) => {
+    const lowerCaseQuery = props.query.toLowerCase();
+    const lowerCaseNickname = props.info.nickname.toLowerCase();
+
+    // in case ubisoft returns a ranked array with no data
+    let rank = 'Error: No Rank'
+    if (props.info.ranked) {
+        rank = props.info.ranked[0].rank_text
+    }
+
     return (
-        <Link className={styles.item} href={`/player/${props.profileId}`}>
-            <div className={styles.img_wrapper}>
+        <Link className={styles.item} href={`/player/${props.info.profileId}`}>
+            <div className={`${styles.img_wrapper} ${lowerCaseNickname == lowerCaseQuery && styles.exactmatch}`}>
                 <div className={styles.img_bg}></div>
-                <UbisoftProfileImage profileId={props.profileId} large />
+                <UbisoftProfileImage profileId={props.info.profileId} large />
                 <div className={styles.info}>
-                    <p className={styles.level}>{ `Level ${props.level}`}</p>
-                    <p className={styles.time}>{timeAgo(props.lastSeen)}</p>
+                    <p className={styles.info_text}>{ `Level ${props.info.level}`}</p>
+                    <p className={styles.info_text}>{timeAgo(props.info.lastSeen)}</p>
                 </div>
+                {!lowerCaseNickname.includes(lowerCaseQuery) && <p className={styles.previousalias}>Previous Alias</p>}
             </div>
-            <p className={styles.rank}>{props.ranked[0].rank_text}</p>
-            <p className={styles.name}>{props.nickname}</p>
+            <p className={styles.rank}>{rank}</p>
+            <p className={styles.name}>{props.info.nickname}</p>
         </Link>
     )
 }
