193e5cc81a2bbead00dab204f87119f71062b1ce
Elias <eeschmoelz@gmail.com>
2023-03-15 23:18:30 +0000

    feat: filter utils func

---
+++ utils/Filter/index.ts
@@ -0,0 +1,66 @@
+import type { FilterCondition, SortCriteria } from "@/types/Filter";
+import type { Hit } from "@/types/SearchResults";
+
+const getValueByPath = (obj: any, path: string) => {
+    const parts = path.split('.');
+    let value = obj;
+    for (let part of parts) {
+      value = value[part];
+      if (value === undefined) {
+        return undefined;
+      }
+    }
+    return value;
+};
+  
+export const filterAndSortData = (
+    data: Hit[],
+    filterConditions: FilterCondition[],
+    sortCriteria: SortCriteria[]
+) => {
+    const filterData = (player: Hit, condition: FilterCondition) => {
+        const value = getValueByPath(player, condition.field);
+        switch (condition.type) {
+        case "text":
+            return value === condition.value;
+        case "number":
+            return value === condition.value;
+        case "range":
+            return (
+                value >= (condition.value as [number, number])[0] &&
+                value <= (condition.value as [number, number])[1]
+            );
+        default:
+            return true;
+        }
+    };
+  
+    let filteredData = data.filter((player) => {
+      return filterConditions.every((condition) => filterData(player, condition));
+    });
+  
+    filteredData.sort((a, b) => {
+        for (let criteria of sortCriteria) {
+            const aValue = getValueByPath(a, criteria.field);
+            const bValue = getValueByPath(b, criteria.field);
+    
+            if (criteria.direction === "asc") {
+                if (aValue > bValue) return 1;
+                if (aValue < bValue) return -1;
+            } else {
+                if (aValue < bValue) return 1;
+                if (aValue > bValue) return -1;
+            }
+        }
+        return 0;
+    });
+  
+    return filteredData;
+};
+
+export const minAndMax = (data: any[], field: any) => {
+    const fieldValues = data.map(player => player[field]);
+    const min = Math.min.apply(null, fieldValues);
+    const max = Math.max.apply(null, fieldValues);
+    return [min, max];
+}
