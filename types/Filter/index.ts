72d16b71b0ed8aac60a8644b2fd67d160e76d8ea
Elias <eeschmoelz@gmail.com>
2023-03-16 23:01:00 +0000

    fix: add name to identify type of filter

--- types/Filter/index.ts
+++ types/Filter/index.ts
@@ -1,9 +1,11 @@
 export type SortCriteria = {
+    name: string;
     field: string;
     direction: "asc" | "desc";
 }
 
 export type FilterCondition = {
+    name: string;
     field: string;
     value: string | number | [number, number];
     type: "text" | "number" | "range";
