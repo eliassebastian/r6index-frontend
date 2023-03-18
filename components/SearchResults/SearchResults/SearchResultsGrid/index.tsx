fd0a44079fa1905ba8f652d5ef377cb437c4abad
Elias <eeschmoelz@gmail.com>
2023-03-18 22:33:54 +0000

    feat: filter chips for grid results

--- components/SearchResults/SearchResults/SearchResultsGrid/index.tsx
+++ components/SearchResults/SearchResults/SearchResultsGrid/index.tsx
@@ -5,7 +5,8 @@ import Notification from '@/components/Notification';
 import { FilterContext } from '@/context/FilterContext';
 import { SearchResult } from '@/types/SearchResults';
 import { filterAndSortData } from '@/utils/Filter';
-import { useContext } from 'react';
+import { useCallback, useContext } from 'react';
+import FilterChip from '../../Filter/FilterChip';
 import SearchResultsGridItem from '../SearchResultsGridItem';
 import styles from './SearchResultsGrid.module.scss';
 
@@ -14,17 +15,52 @@ interface SearchResultsGridProps {
 }
 
 const SearchResultsGrid = ({ results }: SearchResultsGridProps) => {
-
-    const { filters } = useContext(FilterContext);
+    const { filters, setFilters } = useContext(FilterContext);
     const filteredData = filterAndSortData( results.data.hits, filters.filterConditions, filters.sortCriteria );
+    const filterCount = filters.filterConditions.length + filters.sortCriteria.length;
 
-    //console.log(results);
+    const removeFilterCondition = useCallback((idToRemove: string) => {
+        setFilters(prevState => ({
+            ...prevState,
+            filterConditions: prevState.filterConditions.filter(condition => condition.id !== idToRemove),
+        }));
+    }, []);
+      
+    const removeSortCriteria = useCallback((idToRemove: string) => {
+        setFilters(prevState => ({
+            ...prevState,
+            sortCriteria: prevState.sortCriteria.filter(criteria => criteria.id !== idToRemove),
+        }));
+    }, []);
+
+    // console.log(results);
     return (
         <div className={styles.container}>
             <div className={styles.info}>
                 <div className={styles.filters}>
                     <h3 className={styles.h3}>Filters</h3>
-                    <span className={styles.filter_nr}>{ filters.filterConditions.length + filters.sortCriteria.length }</span>
+                    <span className={styles.filter_nr}>{ filterCount }</span>
+                    <div className={styles.overflow}>
+                        {
+                            filterCount > 0 && 
+                            <ul className={styles.chips}>
+                                {filters.filterConditions.map((condition) => 
+                                    <FilterChip 
+                                        id={condition.id} 
+                                        name={condition.name} 
+                                        value={condition.value} 
+                                        onDelete={removeFilterCondition}  
+                                />)}
+                                {filters.sortCriteria.map((criteria) => 
+                                    <FilterChip 
+                                        id={criteria.id}
+                                        name={criteria.name}
+                                        value={criteria.direction}  
+                                        onDelete={removeSortCriteria}  
+                                />)}
+                            </ul>
+                        }
+                    </div>
                 </div>
                 <h3 className={`${styles.h3} ${styles.results_count}`}>{ filteredData.length } Results Found ({ (results.duration/1000).toFixed(2) } seconds)</h3>
             </div>
