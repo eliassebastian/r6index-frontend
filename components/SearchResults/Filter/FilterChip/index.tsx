fcb73c05acb1c7c71aa4152546d131eb3145aac0
Elias <eeschmoelz@gmail.com>
2023-03-18 21:18:54 +0000

    feat: filter chip component

---
+++ components/SearchResults/Filter/FilterChip/index.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import styles from './FilterChip.module.scss';
+
+interface FilterChipProps {
+    id: string;
+    name: string;
+    value: any;
+    onDelete: (idToRemove: string) => void;
+}
+
+const FilterChip = (props: FilterChipProps) => {
+    const value = props.name + ": " + (Array.isArray(props.value) ? `Between ${props.value[0]} & ${props.value[1]}` : props.value);
+
+    const onDelete = (e: React.MouseEvent<HTMLSpanElement, MouseEvent>) => {
+        e.stopPropagation();
+        props.onDelete(props.id);
+    }
+
+    return (
+        <button className={styles.button} onClick={onDelete}>
+            <span className={styles.text}>{value}</span>
+            <span className={styles.icon}>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                    <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
+                </svg>
+            </span>
+        </button>
+    )
+}
+
+export default FilterChip;
