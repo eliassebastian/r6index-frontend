909bcccd83bc4c70b4182c2f4fc95eb664aa9ffc
Elias <eeschmoelz@gmail.com>
2023-03-18 19:03:21 +0000

    feat: filter range slider component

---
+++ components/SearchResults/Filter/FilterSlider/index.tsx
@@ -0,0 +1,154 @@
+'use client';
+
+import { FilterCondition } from "@/types/Filter";
+import { SliderUnstyled, sliderUnstyledClasses } from "@mui/base";
+import { ReactNode, SyntheticEvent, useState } from "react";
+import styles from "./FilterSlider.module.scss";
+
+interface FilterSliderProps {
+    name: string;
+    index: number;
+    field: string;
+    title: string;
+    value: [number, number];
+    currentValue: any;
+    minMax: [number, number];
+    onAdd: (newCriteria: any) => void;
+    onRemove: (idToRemove: string) => void;
+}
+
+const css = `
+    .slider {
+        color: #20b2aa;
+        height: 30px;
+        width: 100%;
+        display: inline-block;
+        position: relative;
+        margin: 0 auto;
+        cursor: pointer;
+        touch-action: none;
+        opacity: 0.8;
+        -webkit-tap-highlight-color: transparent;
+        transition: opacity 0.2s;
+    }
+
+    .slider:hover {
+        opacity: 1;
+    }
+
+    .slider .${sliderUnstyledClasses.rail} {
+        display: block;
+        position: absolute;
+        width: 100%;
+        height: 3px;
+        border-radius: 2px;
+        background-color: black;
+        opacity: 0.5;
+    }
+
+    .slider .${sliderUnstyledClasses.track} {
+        display: block;
+        position: absolute;
+        height: 3px;
+        border-radius: 2px;
+        background-color: black;
+    }
+
+    .slider .${sliderUnstyledClasses.thumb} {
+        position: absolute;
+        width: 16px;
+        height: 16px;
+        margin-left: -6px;
+        margin-top: -6px;
+        box-sizing: border-box;
+        border-radius: 50%;
+        outline: 0;
+        background-color: #732c2f;
+    }
+
+    .slider .label {
+        background: unset;
+        background-color: #3c3c3c;
+        display: flex;
+        padding: .5rem;
+        visibility: hidden;
+        color: #fff;
+        border-radius: .5rem;
+        position: absolute;
+        transform: translate(-35%, -140%) scale(0);
+        transition: transform 0.3s ease;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        white-space: nowrap;
+    }
+
+    .slider:hover .label {
+        visibility: visible;
+        transform: translate(-35%, -140%) scale(1);
+    }
+
+    .slider:hover .value {
+        text-align: center;
+    }
+
+`
+
+const SliderValueLabel = ({ children }: { children: ReactNode }) => {
+    return (
+      <span className="label">
+        <div className="value">{children}</div>
+      </span>
+    );
+}
+
+const FilterSlider = (props: FilterSliderProps) => {
+    const [value, setValue] = useState<number[]>(Array.isArray(props.currentValue) ? props.currentValue : props.value);
+    const minDistance = props.minMax[0] / 10;
+    const id = props.name + props.index;
+
+    const onChange = (event: Event, newValue: number | number[], activeThumb: number) => {
+        if (!Array.isArray(newValue)) return;    
+        if (activeThumb === 0) {
+            setValue([Math.min(newValue[0], value[1] - minDistance), value[1]]);
+        } else {
+            setValue([value[0], Math.max(newValue[1], value[0] + minDistance)]);
+        }
+    }
+
+    const onChangeCommited = (event: Event | SyntheticEvent<Element, Event>, value: number | number[]) =>{
+        if (!Array.isArray(value)) return; 
+        if (value[0] === props.minMax[0] && value[1] === props.minMax[1]) {
+            props.onRemove(id);
+        } else {
+            props.onRemove(id);
+            props.onAdd({
+                id: id,
+                name: props.name,
+                field: props.field,
+                value: value as [number, number],
+                type: "range"
+            } satisfies FilterCondition)
+        }
+    }
+
+    return (
+        <div className={styles.wrapper}>
+            <style type="text/css">{css}</style>
+            <SliderUnstyled 
+                className="slider"
+                value={value}
+                min={props.minMax[0]}
+                max={props.minMax[1]}
+                onChange={onChange}
+                step={props.minMax[0] / 10}
+                slots={{ valueLabel: SliderValueLabel }}
+                valueLabelFormat={(value) => `${value.toFixed(2)} ${props.title}`}
+                disableSwap
+                onChangeCommitted={onChangeCommited}
+            />
+        </div>
+    )
+}
+
+export default FilterSlider
