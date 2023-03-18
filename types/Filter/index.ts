13aabd10416c0a284902cc605d2c220119c59c8f
Elias <eeschmoelz@gmail.com>
2023-03-18 19:02:07 +0000

    fix: sort criteria id field

--- types/Filter/index.ts
+++ types/Filter/index.ts
@@ -1,12 +1,14 @@
 export type SortCriteria = {
+    id: string; //unique id of the sort
     name: string;
     field: string;
     direction: "asc" | "desc";
 }
 
 export type FilterCondition = {
-    name: string;
-    field: string;
-    value: string | number | [number, number];
-    type: "text" | "number" | "range";
+    id: string; //unique id of the filter
+    name: string; //name of the filter
+    field: string; //object path/field
+    value: string | number | [number, number]; //value to filter
+    type: "text" | "number" | "range"; //type of filter
 }
