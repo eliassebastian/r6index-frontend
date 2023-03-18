bb351e50bbd0a9cc4778c1b06420da38d0feb5a8
Elias <eeschmoelz@gmail.com>
2023-03-18 14:34:49 +0000

    fix: filter by field + ensure > 1 cond is met

--- utils/Filter/index.ts
+++ utils/Filter/index.ts
@@ -1,18 +1,6 @@
 import type { FilterCondition, SortCriteria } from "@/types/Filter";
 import type { Hit } from "@/types/SearchResults";
 
-const getValueByPath = (obj: any, path: string) => {
-    const parts = path.split('.');
-    let value = obj;
-    for (let part of parts) {
-      value = value[part];
-      if (value === undefined) {
-        return undefined;
-      }
-    }
-    return value;
-};
-
 const getNestedProperty = (obj: any, path: string) => {
     const properties = path.split('.');
     let currentObj = obj;
@@ -26,37 +14,52 @@ const getNestedProperty = (obj: any, path: string) => {
 
     return currentObj;
 }
-  
+
 export const filterAndSortData = (
     data: Hit[],
     filterConditions: FilterCondition[],
     sortCriteria: SortCriteria[]
-) => {
-    const filterData = (player: Hit, condition: FilterCondition) => {
-        const value = getValueByPath(player, condition.field);
-        switch (condition.type) {
-        case "text":
-            return value === condition.value;
-        case "number":
-            return value === condition.value;
-        case "range":
-            return (
-                value >= (condition.value as [number, number])[0] &&
-                value <= (condition.value as [number, number])[1]
-            );
-        default:
-            return true;
-        }
+  ) => {
+    const groupedConditions = filterConditions.reduce<Record<string, FilterCondition[]>>(
+        (groups, condition) => {
+            if (!groups[condition.field]) {
+                groups[condition.field] = [];
+            }
+            groups[condition.field].push(condition);
+            return groups;
+        },
+        {}
+    );
+  
+    const filterData = (player: Hit, conditions: FilterCondition[]) => {
+        return conditions.some((condition) => {
+            const value = getNestedProperty(player, condition.field);
+            switch (condition.type) {
+                case "text":
+                    return value === condition.value;
+                case "number":
+                    return value === condition.value;
+                case "range":
+                    return (
+                        value >= (condition.value as [number, number])[0] &&
+                        value <= (condition.value as [number, number])[1]
+                    );
+                default:
+                    return true;
+            }
+        });
     };
   
-    let filteredData = data.filter((player) => {
-      return filterConditions.every((condition) => filterData(player, condition));
+    const filteredData = data.filter((player) => {
+        return Object.values(groupedConditions).every((conditions) =>
+            filterData(player, conditions)
+        );
     });
   
     filteredData.sort((a, b) => {
         for (let criteria of sortCriteria) {
-            const aValue = getValueByPath(a, criteria.field);
-            const bValue = getValueByPath(b, criteria.field);
+            const aValue = getNestedProperty(a, criteria.field);
+            const bValue = getNestedProperty(b, criteria.field);
     
             if (criteria.direction === "asc") {
                 if (aValue > bValue) return 1;
@@ -68,10 +71,10 @@ export const filterAndSortData = (
         }
         return 0;
     });
-  
+
     return filteredData;
 };
-
+  
 export const minAndMax = (data: any[], field: string) => {
     const fieldValues = data.map(item => getNestedProperty(item, field))
     const min = Math.min.apply(null, fieldValues);
