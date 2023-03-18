5cb1e5a89cc4c606c86a1e316fa11f124a1f9b3f
Elias <eeschmoelz@gmail.com>
2023-03-18 21:20:36 +0000

    feat: new filters

--- components/SearchResults/Filter/Filter/index.tsx
+++ components/SearchResults/Filter/Filter/index.tsx
@@ -1,29 +1,30 @@
 'use client';
 
-import { FilterContext } from "@/context/FilterContext";
-import type { FilterCondition, SortCriteria } from "@/types/Filter";
 import type { Hit } from "@/types/SearchResults";
 import { minAndMax, uniqueFieldValues } from "@/utils/Filter";
-import { useCallback, useContext } from "react";
-import FilterAccordion from "../FilterAccordion";
-import FilterCheckbox from "../FilterCheckbox";
 import FilterSection from "../FilterSection";
 
 interface FilterProps {
     results: Hit[];
 }
 
-
 const Filter = ({ results }: FilterProps) => {
     const getPlatforms = uniqueFieldValues( results, 'platform' );
     const getKDs = uniqueFieldValues( results, 'ranked.0.kill_death_ratio' );
     const getRanks = uniqueFieldValues( results, 'ranked.0.rank_text' );
-    const minMaxs = minAndMax( results, 'ranked.0.kill_death_ratio' );
-
-    console.log(getPlatforms, getKDs, minMaxs, getRanks);
+    const getWL = minAndMax( results, 'ranked.0.win_lose_ratio' );
+    const getLevels = minAndMax( results, 'level' );
 
     return (
         <div>
+            <FilterSection
+                name={"platform"}
+                title={"Platform"}
+                type={"text"}
+                icon={"/icons/rank.svg"}
+                field={"platform"}
+                data={getPlatforms}  
+            />
             <FilterSection
                 name={"rank"}
                 title={"Rank"}
@@ -34,12 +35,13 @@ const Filter = ({ results }: FilterProps) => {
                 sort={"ranked.0.rank"} 
             />
             <FilterSection
-                name={"platform"}
-                title={"Platform"}
-                type={"text"}
+                name={"level"}
+                title={"Level"}
+                type={"range"}
                 icon={"/icons/rank.svg"}
-                field={"platform"}
-                data={getPlatforms}  
+                field={"level"}
+                data={getLevels}
+                sort={"level"}
             />
             <FilterSection
                 name={"kd"}
@@ -49,6 +51,15 @@ const Filter = ({ results }: FilterProps) => {
                 field={"ranked.0.kill_death_ratio"}
                 data={getKDs}
                 sort={"ranked.0.kill_death_ratio"}
+            />
+            <FilterSection
+                name={"wl"}
+                title={"W/L"}
+                type={"range"}
+                icon={"/icons/rank.svg"}
+                field={"ranked.0.win_lose_ratio"}
+                data={getWL}
+                sort={"ranked.0.win_lose_ratio"}
             />
         </div>
     )
