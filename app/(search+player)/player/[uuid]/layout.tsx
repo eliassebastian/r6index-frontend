e1d6546e961ba87e8a54d5aaf8fa59fbb101c2fd
Elias <eeschmoelz@gmail.com>
2023-03-21 20:58:33 +0000

    feat: player layout mobile section

--- app/(search+player)/player/[uuid]/layout.tsx
+++ app/(search+player)/player/[uuid]/layout.tsx
@@ -1,5 +1,7 @@
 import PlayerBanner from '@/components/Player/PlayerBanner/PlayerBanner';
 import PlayerBannerLoading from '@/components/Player/PlayerBanner/PlayerBannerLoading';
+import PlayerBasicInfo from '@/components/Player/PlayerBasicInfo';
+import { PlayerButtonTabMobile } from '@/components/Player/PlayerButtonTab';
 import PlayerFadeBackground from '@/components/Player/PlayerFadeBackground';
 import { Suspense } from 'react';
 import styles from './Layout.module.scss';
@@ -16,7 +18,14 @@ export default function Layout({ children, params }: { children: React.ReactNode
                 </Suspense>
             </div>
             <div className={styles.container}>
-
+                <section className={styles.container_header}>
+                    <div className={styles.full}>
+                        <Suspense fallback={<PlayerBannerLoading/>}>
+                            {/* @ts-expect-error Async Server Component */}
+                            <PlayerBasicInfo id={params.uuid} platform={"uplay"} mobile />
+                        </Suspense>
+                    </div>
+                </section>
             </div>
         </main>
     )
