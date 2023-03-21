30f4de0f5f6d76fa11abb62ec5e2cf4a44f69af9
Elias <eeschmoelz@gmail.com>
2023-03-21 20:55:36 +0000

    feat: player info component

--- components/Player/PlayerBanner/PlayerBanner/index.tsx
+++ components/Player/PlayerBanner/PlayerBanner/index.tsx
@@ -2,6 +2,7 @@ import { operatorsConfig } from '@/configs/Operators';
 import { calculateBestOperator } from '@/utils/Operators';
 import Image from 'next/image';
 import { fetchPlayer } from '../../fetchPlayer';
+import PlayerBasicInfo from '../../PlayerBasicInfo';
 import PlayerTest from '../../PlayerTest';
 import styles from '../PlayerBanner.module.scss';
 
@@ -10,18 +11,20 @@ interface PlayerBannerProps {
 }
 
 const PlayerBanner = async (props: PlayerBannerProps) => {
+
     const player = await fetchPlayer(props.uuid, "uplay");
     const topOperator = calculateBestOperator([...player.data.operators.attacker, ...player.data.operators.defender]);
     const operatorConfig = operatorsConfig[topOperator.statsDetail];
-    
+
     return (
         <section className={styles.banner} style={{ backgroundColor: "#4A4A4A" }}>
             {/* player details */}
             <div className={styles.player}>
-                <PlayerTest data={player.data} />
+                {/* @ts-expect-error Async Server Component */}
+                <PlayerBasicInfo id={props.uuid} platform={"uplay"} />
             </div>
             {/* image */}
-            <Image className={styles.img} src={operatorConfig.large} alt={`${ topOperator.statsDetail } banner image`}  fill priority />
+            <Image className={styles.img} src={operatorConfig.large} alt={`${ topOperator.statsDetail } banner image`} fill priority />
         </section>
     );
 }
