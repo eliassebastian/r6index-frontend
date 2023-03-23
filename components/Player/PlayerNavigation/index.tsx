8cdc1a903200bc7ad9a2f48b3f24cefd613286eb
Elias <eeschmoelz@gmail.com>
2023-03-23 23:32:56 +0000

    feat: player layout navigation

---
+++ components/Player/PlayerNavigation/index.tsx
@@ -0,0 +1,70 @@
+'use client';
+
+import Link from 'next/link';
+import { useSelectedLayoutSegment } from 'next/navigation';
+import { useLayoutEffect, useRef, useState } from 'react';
+import styles from './PlayerNavigation.module.scss';
+
+const PlayerNavigationTabs: { [key: string]: [ string, number ] } = {
+    "overview": [ "Overview", 0 ],
+    "summary": [ "Summary", 1 ],
+    "operators": [ "Operators", 2 ],
+    "maps": [ "Maps", 3 ],
+    "weapons": [ "Weapons", 4 ],
+    "trends": [ "Trends", 5 ]
+}
+
+const PlayerNavigation = (props: { slug: string }) => {
+
+    const navigationRef = useRef<HTMLUListElement>(null);
+    const tabRef = useRef<HTMLDivElement>(null);
+    const selectedSegment = useSelectedLayoutSegment();
+    const href = `/player/${props.slug}`;
+    const [tab, setTab] = useState(0);
+
+    useLayoutEffect(() => {
+        const active = selectedSegment === null ? "overview" : selectedSegment;
+        const tab = PlayerNavigationTabs[active];
+        setTab(tab[1]);
+    }, [selectedSegment])
+
+    // Update tab position
+    useLayoutEffect(() => {
+        if (!tabRef.current || !navigationRef.current) return;
+        const btn = navigationRef.current.children[tab] as HTMLAnchorElement;
+        tabRef.current.style.transform = `translate(${btn.offsetLeft}px, 10px)`;
+        tabRef.current.style.width = `${btn.offsetWidth}px`;
+        tabRef.current.style.height = `${btn.offsetHeight}px`;
+    }, [tab])
+
+    // Overview / Summary / Operators / Maps / Weapons / Trends
+
+    return (
+        <div className={styles.navigation}>
+            <div ref={tabRef} className={styles.background_tab}>
+            </div>
+            <ul ref={navigationRef} className={styles.navigation_list}>
+                <Link href={`${href}`} className={`${ styles.link } ${ selectedSegment === null && styles.active }`} onMouseEnter={() => { setTab(0) }}>
+                    Overview
+                </Link>
+                <Link href={`${href}/summary`} className={`${ styles.link } ${ selectedSegment === "summary" && styles.active }`} onMouseEnter={() => { setTab(1) }}>
+                    Summary
+                </Link>
+                <Link href={`${href}/operators`} className={`${ styles.link } ${ selectedSegment === "operators" && styles.active }`} onMouseEnter={() => { setTab(2) }}>
+                    Operators
+                </Link>
+                <Link href={`${href}/maps`} className={`${ styles.link } ${ selectedSegment === "maps" && styles.active }`} onMouseEnter={() => { setTab(3) }}>
+                    Maps
+                </Link>
+                <Link href={`${href}/weapons`} className={`${ styles.link } ${ selectedSegment === "weapons" && styles.active }`} onMouseEnter={() => { setTab(4) }}>
+                    Weapons
+                </Link>
+                <Link href={`${href}/trends`} className={`${ styles.link } ${ selectedSegment === "trends" && styles.active }`} onMouseEnter={() => { setTab(5) }}>
+                    Trends
+                </Link>
+            </ul>
+        </div>
+    )
+}
+
+export default PlayerNavigation;
