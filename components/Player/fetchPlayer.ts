1f4c1bf98e75917a34739cc5cee1dc0f56d5e6c8
Elias <eeschmoelz@gmail.com>
2023-03-24 22:00:50 +0000

    fix: enable uuid validation

--- components/Player/fetchPlayer.ts
+++ components/Player/fetchPlayer.ts
@@ -2,10 +2,11 @@ import { PlayerResponse } from "@/types/Player";
 import isUbisoftUUID from "@/utils/UbisoftID";
 
 export const fetchPlayer = async (uuid: string, platform: string) => {
-    //if (!isUbisoftUUID(uuid)) throw new Error("Invalid UUID");
+    if (!isUbisoftUUID(uuid)) throw new Error("Invalid UUID");
     const testUuid = "661e632b-c2cd-4994-96ba-6d9ed930b941";
-    const res = await fetch(`http://127.0.0.1:8080/v1/player?p=uplay&id=${ testUuid }`, { next: { revalidate: 3600 } });
+    const res = await fetch(`http://127.0.0.1:8080/v1/player?p=uplay&id=${ uuid }`, { next: { revalidate: 3600 } });
     if (res.status !== 200) throw new Error("Failed to fetch player");
+    
     const data: PlayerResponse = await res.json();
     return data;
 }
