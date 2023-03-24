063bc18eb3c18fc68c481e56da2698c57f4bd349
Elias <eeschmoelz@gmail.com>
2023-03-24 12:28:23 +0000

    fix: use favourite player persistant storage

--- components/Search/SearchFavourites/index.tsx
+++ components/Search/SearchFavourites/index.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { useFavoritePlayerStore } from "@/stores/FavouritePlayers";
 import Image from "next/image";
 /*
 let shuffled = unshuffled
@@ -32,17 +33,11 @@ const FavouriteItem = (props: FavouriteItemProps) => {
 }
 
 const SearchFavourites = () => {
+    const favouritePlayers = useFavoritePlayerStore((state) => state.favouritePlayers);
+
     return (
         <ul className={styles.list}>
-            <FavouriteItem name="spoit.koi" id="ab1ff7ae-13e4-4a6a-9b03-317285f8057b" />
-            <FavouriteItem name="leongids.koi" id="661e632b-c2cd-4994-96ba-6d9ed930b941" />
-            <FavouriteItem name="pengu.dz" id="f1b010e1-d441-47b1-be1f-cea6e2f22e19" />
-            <FavouriteItem name="rath.-" id="eac01e72-a88c-4847-ad73-0e03ce7a1870" />
-            <FavouriteItem name="EddyDTB" id="95171ef3-32d7-466f-a8ea-438b845444e3" />
-            {/* <FavouriteItem name="EddyDTB" id="95171ef3-32d7-466f-a8ea-438b845444e3" />
-            <FavouriteItem name="EddyDTB" id="95171ef3-32d7-466f-a8ea-438b845444e3" />
-            <FavouriteItem name="EddyDTB" id="95171ef3-32d7-466f-a8ea-438b845444e3" />
-            <FavouriteItem name="EddyDTB" id="95171ef3-32d7-466f-a8ea-438b845444e3" /> */}
+            {favouritePlayers.map((player) => <FavouriteItem key={player.id} name={player.name} id={player.id} />)}
         </ul>
     )
 }
