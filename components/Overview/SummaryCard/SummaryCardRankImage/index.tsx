9c4daa20a86791e61a0d00cc085da10d130878f5
Elias <eeschmoelz@gmail.com>
2023-03-27 18:20:27 +0100

    fix: use css var setproperty

--- components/Overview/SummaryCard/SummaryCardRankImage/index.tsx
+++ components/Overview/SummaryCard/SummaryCardRankImage/index.tsx
@@ -5,48 +5,53 @@ import styles from "./SummaryCardRankImage.module.scss";
 interface SummaryCardRankImageProps {
     rank: number;
     topPosition: number;
+    img: string;
+    gradient: string;
 }
 
 const SummaryCardRankImage = (props: SummaryCardRankImageProps) => {
     const ref = useRef<HTMLDivElement>(null);
     const imageRef = useRef<HTMLImageElement>(null);
+    const imgSrc = props.rank === 36 && props.topPosition !== 0 ? `https://trackercdn.com/cdn/r6.tracker.network/ranks/champion-ranks/${props.topPosition}.png?v=3` : props.img
 
     const onMouseMove = (e: MouseEvent<HTMLDivElement>) => {
         if (!ref.current || !imageRef.current) return;
 
         const { clientX, clientY } = e;
 
-        var rect = ref.current.getBoundingClientRect();
-        var x = clientX - rect.left;
-        var y = clientY - rect.top;
+        const rect = ref.current.getBoundingClientRect();
+        const x = clientX - rect.left;
+        const y = clientY - rect.top;
+        const centerX = rect.width / 2;
+        const centerY = rect.height / 2;
 
-        var centerX = rect.width / 2;
-        var centerY = rect.height / 2;
+        const deltaX = (x - centerX) / centerX;
+        const deltaY = (y - centerY) / centerY;
 
-        var deltaX = (x - centerX) / centerX;
-        var deltaY = (y - centerY) / centerY;
+        const rotationStrength = 20;
+        const rotationY = rotationStrength * deltaX;
+        const rotationX = -rotationStrength * deltaY;
 
-        var rotationStrength = 20;
-        var rotationY = rotationStrength * deltaX;
-        var rotationX = -rotationStrength * deltaY;
-
-        var translationStrength = 10;
-        var translationX = translationStrength * deltaX;
-        var translationY = translationStrength * deltaY;
-
-        ref.current.style.transform = `translate(${translationX}px, ${translationY}px) rotateX(${rotationX}deg) rotateY(${rotationY}deg)`;
-        imageRef.current.style.transform = `perspective(1000) translate(${translationX}px, ${translationY}px) rotateX(${rotationX}deg) rotateY(${rotationY}deg) scale(1.1)`;
+        const translationStrength = 10;
+        const translationX = translationStrength * deltaX;
+        const translationY = translationStrength * deltaY;
+        
+        ref.current.style.setProperty('--rotationX', `${rotationX}deg`);
+        ref.current.style.setProperty('--rotationY', `${rotationY}deg`);
+        ref.current.style.setProperty('--translationX', `${translationX}px`);
+        ref.current.style.setProperty('--translationY', `${translationY}px`);
     }
 
-    const onMouseLeave = () => {
-        if (!ref.current || !imageRef.current) return;
-        ref.current.style.transform = `perspective(1000px) translate(0px, 0px) rotateX(0deg) rotateY(0deg)`;
-        imageRef.current.style.transform = `perspective(1000px) translate(0px, 0px) rotateX(0deg) rotateY(0deg) scale(1)`;
+    const onLoadingComplete = (img: HTMLImageElement) => {
+        img.style.opacity = "1";
     }
 
     return (
-        <div className={styles.image} ref={ref} onMouseMove={onMouseMove} onMouseLeave={onMouseLeave}>
-            <Image ref={imageRef} src={"https://trackercdn.com/cdn/r6.tracker.network/ranks/champion-ranks/1000.png?v=3"} width={64} height={64} alt={"current rank"}/>
+        <div className={styles.image} ref={ref} onMouseMove={onMouseMove}>
+            <div style={{ background: props.gradient }} className={styles.glow}></div>
+            <div ref={imageRef} className={styles.img_wrapper}>
+                <Image src={imgSrc} fill alt={"current rank"} onLoadingComplete={onLoadingComplete}/>
+            </div>
         </div>
     )
 }
