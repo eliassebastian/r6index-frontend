e913a06fb5b20e6eea01bb1b6c932ebfe7a29553
Elias <eeschmoelz@gmail.com>
2023-03-28 22:12:22 +0100

    feat: wrap indiv cmpts in error b and suspense

---
+++ app/(search+player)/player/[uuid]/page.tsx
@@ -0,0 +1,42 @@
+import ErrorBoundary from '@/components/ErrorBoundary';
+import MapsCard from '@/components/Overview/Map';
+import OperatorsCard from '@/components/Overview/Operators';
+import OverviewCard from '@/components/Overview/OverviewCard';
+import OverviewCardError from '@/components/Overview/OverviewCardError';
+import SummaryCard from '@/components/Overview/SummaryCard';
+import TrendsCard from '@/components/Overview/Trends';
+import WeaponsCard from '@/components/Overview/Weapons';
+import { Suspense } from 'react';
+import styles from './OverviewPage.module.scss';
+
+export default function Overview({ params }: { params: { uuid: string } }) {
+    return (
+        <div className={styles.page}>
+            {/* <SummaryCard uuid={params.uuid}/>
+            <WeaponsCard uuid={params.uuid}/>
+            <OperatorsCard uuid={params.uuid}/>
+            <MapsCard uuid={params.uuid}/>
+            <TrendsCard uuid={params.uuid}/> */}
+            <OverviewCard uuid={params.uuid} path={"summary"}>
+                <ErrorBoundary fallback={<OverviewCardError />} >
+                    <Suspense>
+                        {/* @ts-expect-error Async Server Component */}
+                        <SummaryCard uuid={params.uuid} />
+                    </Suspense>
+                </ErrorBoundary>
+            </OverviewCard>
+            <OverviewCard uuid={params.uuid} path={"weapons"}>
+
+            </OverviewCard>
+            <OverviewCard uuid={params.uuid} path={"operators"}>
+
+            </OverviewCard>
+            <OverviewCard uuid={params.uuid} path={"maps"}>
+
+            </OverviewCard>
+            <OverviewCard uuid={params.uuid} path={"trends"}>
+
+            </OverviewCard>
+        </div>
+    )
+}
