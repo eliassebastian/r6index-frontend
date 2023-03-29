913337ace2c574d508bc9de03513f2247206007e
Elias <eeschmoelz@gmail.com>
2023-03-29 19:42:41 +0100

    feat: calculate top map util func

--- utils/Ubisoft/index.ts
+++ utils/Ubisoft/index.ts
@@ -1,3 +1,4 @@
+import { Map } from "@/types/Maps";
 import { Operator } from "@/types/Operators";
 import { Weapon } from "@/types/Weapons";
 
@@ -17,18 +18,34 @@ export const calculateBestOperator = (operators: Operator[]) => {
     }, { ...operators[0], score: -Infinity });
 }
 
+
+const calculateWeaponScore = (w: Weapon) =>
+    w.roundsWon * 1 +
+    w.roundsPlayed * 0.5 +
+    w.kills * 0.5 +
+    w.headshots * 2 +
+    w.headshotAccuracy * 10 +
+    w.roundsWithAKill * 5 +
+    w.roundsWithMultiKill * 5;
+
 // get the best weapon for a player based on their individual stats
 export const calculateBestWeapon = (weapons: Weapon[]) => {
     return weapons.reduce((bestWeapon, weapon) => {
-        const calculateScore = (w: Weapon) =>
-            w.roundsWon * 1 +
-            w.roundsPlayed * 0.5 +
-            w.kills * 0.5 +
-            w.headshots * 2 +
-            w.headshotAccuracy * 10 +
-            w.roundsWithAKill * 5 +
-            w.roundsWithMultiKill * 5;
+        return calculateWeaponScore(weapon) > calculateWeaponScore(bestWeapon) ? weapon : bestWeapon;
+    });
+}
 
-        return calculateScore(weapon) > calculateScore(bestWeapon) ? weapon : bestWeapon;
+const calculateMapScore = (map: Map) => {
+    const minutesPlayedScore = map.minutesPlayed * .2;
+    const roundsPlayedScore = (map.roundsWon / map.roundsPlayed) * .2;
+    const matchesPlayedScore = (map.matchesWon / map.matchesPlayed) * .3;
+    const kostScore = map.roundsWithKOST.value * .3;
+
+    return minutesPlayedScore + roundsPlayedScore + matchesPlayedScore + kostScore;
+}
+
+export const calculateBestMap = (maps: Map[]) => {
+    return maps.reduce((topMap, currentMap) => {
+        return calculateMapScore(currentMap) > calculateMapScore(topMap) ? currentMap : topMap;
     });
 }
