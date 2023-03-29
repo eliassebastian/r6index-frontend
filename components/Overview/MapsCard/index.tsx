4256e422354c7290588795bd99e0ae23b96134d2
Elias <eeschmoelz@gmail.com>
2023-03-29 19:40:26 +0100

    feat: maps card component

---
+++ components/Overview/MapsCard/index.tsx
@@ -0,0 +1,61 @@
+import { fetchPlayer } from "@/components/Player/fetchPlayer";
+import { MapConfig } from "@/configs/Maps";
+import { convertNumberToTwoDecimals } from "@/utils/Numbers";
+import { calculateBestMap } from "@/utils/Ubisoft";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./MapsCard.module.scss";
+
+interface MapsCardProps {
+    uuid: string;
+}
+
+const MapsCard = async (props: MapsCardProps) => {
+
+    const { data: { maps } } = await fetchPlayer(props.uuid, "uplay");
+
+    console.log(maps);
+
+    // check if maps is null, if it is, throw an error for error boundary to catch 
+    const isMapsNotAvailable = maps == null;
+    if (isMapsNotAvailable) throw new Error("Maps Data is current unavailable");
+
+    const topMap = calculateBestMap(maps);
+    const mapConfig = MapConfig[topMap.statsDetail];
+    const winRate = topMap.roundsWon / topMap.roundsPlayed * 100;
+
+    return (
+        <Link 
+            href={`/player/${props.uuid}/maps`}
+            className={styles.card}
+        >
+            {/* <div className={styles.image_wrapper}>
+                <Image src={mapConfig.image} alt={`${ topMap.statsDetail } map image`} fill priority />
+            </div> */}
+            <div>
+                <h2 className={styles.title}>TOP MAP</h2>
+                <span className={styles.weaponName}>{topMap.statsDetail}</span>
+            </div>
+            <div className={styles.grid}>
+                <div className={styles.grid_content} >
+                    <span className={styles.grid_text}>{ convertNumberToTwoDecimals( winRate, 1 ) }%</span>
+                    <h3 className={styles.grid_subtitle}>Win Rate</h3>
+                </div>
+                <div className={styles.grid_content}>
+                    <span className={styles.grid_text}>{ convertNumberToTwoDecimals( topMap.roundsWithKOST.value * 100, 1) }%</span>
+                    <h3 className={styles.grid_subtitle}>KOST</h3>
+                </div>
+                <div className={styles.grid_content}>
+                    <span className={styles.grid_text}>{ convertNumberToTwoDecimals( topMap.killDeathRatio.value ) }</span>
+                    <h3 className={styles.grid_subtitle}>K/D</h3>
+                </div>
+                <div className={styles.grid_content}>
+                    <span className={styles.grid_text}>{ convertNumberToTwoDecimals( topMap.killsPerRound.value, 1) }</span>
+                    <h3 className={styles.grid_subtitle}>KPR</h3>
+                </div>
+            </div>
+        </Link>
+    )
+}
+
+export default MapsCard;
