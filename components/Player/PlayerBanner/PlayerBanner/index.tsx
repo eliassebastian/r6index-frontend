dd4ca0ec4bdffcacb3507890e8cf38455a1be4bf
Elias <eeschmoelz@gmail.com>
2023-03-30 15:44:19 +0100

    fix: load operator info if operator data is valid

--- components/Player/PlayerBanner/PlayerBanner/index.tsx
+++ components/Player/PlayerBanner/PlayerBanner/index.tsx
@@ -1,8 +1,10 @@
 import { operatorsConfig } from '@/configs/Operators';
-import { calculateBestOperator } from '@/utils/Operators';
+import { calculateBestOperator } from '@/utils/Ubisoft';
 import Image from 'next/image';
+import { Suspense } from 'react';
 import { fetchPlayer } from '../../fetchPlayer';
 import PlayerBasicInfo from '../../PlayerBasicInfo';
+import PlayerBasicInfoLoading from '../../PlayerBasicInfo/Loading';
 import PlayerTest from '../../PlayerTest';
 import styles from '../PlayerBanner.module.scss';
 
@@ -11,20 +13,23 @@ interface PlayerBannerProps {
 }
 
 const PlayerBanner = async (props: PlayerBannerProps) => {
+    const { data: { operators } } = await fetchPlayer(props.uuid, "uplay");
 
-    const player = await fetchPlayer(props.uuid, "uplay");
-    const topOperator = calculateBestOperator([...player.data.operators.attacker, ...player.data.operators.defender]);
-    const operatorConfig = operatorsConfig[topOperator.statsDetail];
+    const topOperator = operators && calculateBestOperator([...operators.attacker, ...operators.defender]);
+    const operatorConfig = operators && operatorsConfig[topOperator.statsDetail];
 
     return (
         <section className={styles.banner}>
+            {/* <PlayerTest data={player} /> */}
             {/* player details */}
             <div className={styles.player}>
-                {/* @ts-expect-error Async Server Component */}
-                <PlayerBasicInfo id={props.uuid} platform={"uplay"} />
+                <Suspense fallback={<PlayerBasicInfoLoading/>}>
+                    {/* @ts-expect-error Async Server Component */}
+                    <PlayerBasicInfo id={props.uuid} platform={"uplay"} />
+                </Suspense>
             </div>
             {/* image */}
-            <Image className={styles.img} src={operatorConfig.large} alt={`${ topOperator.statsDetail } banner image`} fill priority />
+            {operators && <Image className={styles.img} src={operatorConfig.large} alt={`${ topOperator.statsDetail } banner image`} fill priority />}
         </section>
     );
 }
