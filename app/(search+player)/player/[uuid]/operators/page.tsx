000fcd95fdd897f4301570a544b8711f8cede5c1
Elias <eeschmoelz@gmail.com>
2023-03-31 01:57:16 +0100

    feat: player operators page

---
+++ app/(search+player)/player/[uuid]/operators/page.tsx
@@ -0,0 +1,12 @@
+export const dynamic = 'force-static', dynamicParams = true;
+export function generateStaticParams() {
+    return [];
+}
+
+export default function Operators() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
