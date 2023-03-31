fae576a01551250ef913e2e57bbfa681370e082c
Elias <eeschmoelz@gmail.com>
2023-03-31 21:55:37 +0100

    fix: scroll position on home route change

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,13 @@ import HomeAction from '@/components/Home/HomeAction'
 import styles from './page.module.scss'
 
 import HomeBackground from '@/components/Backgrounds/BackgroundHome'
-
+import PlayerScroll from '@/components/Player/PlayerScroll'
+//      {/* <FooterHome/> */}
 export default function Home() {
   return (
     <>
       <main className={styles.main}>
+        <PlayerScroll/>
         <HomeBackground/>
           {/* <div className={styles.gradient}></div> */}
         <HomeAction/>
