cdb6c47d3535bdf2d25cad0feb4f2f18c3524318
Elias <eeschmoelz@gmail.com>
2023-03-31 01:57:42 +0100

    feat: player weapons page

---
+++ app/(search+player)/player/[uuid]/weapons/page.tsx
@@ -0,0 +1,12 @@
+export const dynamic = 'force-static', dynamicParams = true;
+export function generateStaticParams() {
+    return [];
+}
+
+export default function Weapons() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
