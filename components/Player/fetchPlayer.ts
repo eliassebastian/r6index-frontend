0bf2fb593512e0576857f937f1a7a56456919976
Elias <eeschmoelz@gmail.com>
2023-03-31 01:58:00 +0100

    fix: force cache for revalidation pattern

--- components/Player/fetchPlayer.ts
+++ components/Player/fetchPlayer.ts
@@ -3,10 +3,10 @@ import isUbisoftUUID from "@/utils/UbisoftID";
 
 export const fetchPlayer = async (uuid: string, platform: string) => {
     if (!isUbisoftUUID(uuid)) throw new Error("Invalid UUID");
-    const testUuid = "661e632b-c2cd-4994-96ba-6d9ed930b941";
-    const res = await fetch(`http://127.0.0.1:8080/v1/player?p=uplay&id=${ uuid }`, { next: { revalidate: 3600 } });
-    if (res.status !== 200) throw new Error("Failed to fetch player");
+    const res = await fetch(`http://127.0.0.1:8080/v1/player?p=uplay&id=${ uuid }`);
     
+    if (res.status !== 200) throw new Error("Failed to Get Player Information");
+
     const data: PlayerResponse = await res.json();
     return data;
 }
