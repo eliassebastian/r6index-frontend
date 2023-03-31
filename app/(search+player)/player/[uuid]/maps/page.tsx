c8dc2b68e8e6eaf8763a77b2ec6f6755a020465d
Elias <eeschmoelz@gmail.com>
2023-03-31 01:57:07 +0100

    feat: player maps page

---
+++ app/(search+player)/player/[uuid]/maps/page.tsx
@@ -0,0 +1,12 @@
+export const dynamic = 'force-static', dynamicParams = true;
+export function generateStaticParams() {
+    return [];
+}
+
+export default function Maps() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
