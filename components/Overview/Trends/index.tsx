46dd94e2b0f374bc1eea5a69b95d421dd8034d5e
Elias <eeschmoelz@gmail.com>
2023-04-02 21:54:23 +0100

    feat: trends performance graph

--- components/Overview/Trends/index.tsx
+++ components/Overview/Trends/index.tsx
@@ -1,8 +1,9 @@
-import { fetchPlayer } from "@/components/Player/fetchPlayer";
 import Link from "next/link";
 import TrendsCardContent from "./TrendsCardContent";
 import styles from "./TrendsCard.module.scss";
 import OverviewEmpty from "../OverviewEmpty";
+import { fetchPlayer } from "@/lib/api/fetchPlayer";
+import { convertNumberToTwoDecimals } from "@/utils/Numbers";
 
 interface MapsCardProps {
     uuid: string;
@@ -12,17 +13,28 @@ const TrendsCard = async (props: MapsCardProps) => {
     const { data: { trends } } = await fetchPlayer(props.uuid, "uplay");
 
     const isTrendsNotAvailable = trends == null;
-    //const topOperator = maps && calculateBestOperator([...operators.attacker, ...operators.defender])
+    const kostTrend = !isTrendsNotAvailable && trends.trends.find(trend => trend.name === "roundsWithKOST");
 
-    //TODO: return empty div if data is not available
-    if (isTrendsNotAvailable) return <OverviewEmpty message="Performance Data Currently Unavailable" subtitle="This could be due to not enough games played, New Season Starting, or Ubisoft Server Issues."/>;
+    //trends data is not available
+    if (isTrendsNotAvailable || !kostTrend) return <OverviewEmpty message="Performance Data Currently Unavailable" subtitle="This could be due to not enough games played, New Season Starting, or Ubisoft Server Issues."/>;
+
+    console.log(kostTrend, kostTrend.trend, kostTrend.average, kostTrend.low, kostTrend.high )
 
     return (
         <Link 
             href={`/player/${props.uuid}/trends`}
             className={styles.card}
-        >  
-            <TrendsCardContent uuid={props.uuid} />
+        >
+            <h3 className={styles.title}>PERFORMANCE</h3>
+            <TrendsCardContent uuid={props.uuid} data={ kostTrend.trend } average={ kostTrend.average } />
+            <div className={styles.grid}>
+                <div>
+                    <span className={styles.stat_title}>Low</span><span className={styles.stat}>{ convertNumberToTwoDecimals(Math.min(...kostTrend.trend), 2) }</span>
+                </div>
+                <div>
+                    <span className={styles.stat_title}>High</span><span className={styles.stat}>{ convertNumberToTwoDecimals(Math.max(...kostTrend.trend), 2) }</span>
+                </div>
+            </div>
         </Link>
     )
 }
