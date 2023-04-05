be3d57a6021a98f15c760f7194b68723706e70bb
Elias <eeschmoelz@gmail.com>
2023-04-05 20:55:56 +0100

    feat: operators grid component

---
+++ components/Operators/OperatorsGrid/index.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { FilterContext } from "@/context/FilterContext";
+import { Operator, OperatorSide } from "@/types/Operators"
+import { filterAndSortData } from "@/utils/Filter";
+import { useContext } from "react";
+import OperatorsGridItem from "../OperatorsGridItem";
+import styles from "./OperatorsGrid.module.scss";
+
+interface OperatorsGridProps {
+    data: Operator[];
+}
+
+const OperatorsGrid = ({ data }: OperatorsGridProps) => {
+    const { filters, setFilters } = useContext( FilterContext );
+    //const filteredData = filterAndSortData( operators, filters.filterConditions, filters.sortCriteria );
+
+    console.log("count", data.length)
+
+    return (
+        <div className={styles.grid}>
+            {
+                data.map((operator) => <OperatorsGridItem key={operator.statsDetail} data={operator} />)
+            }
+        </div>
+    )
+}
+
+export default OperatorsGrid
