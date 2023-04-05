0fc525f29095e7cdc739d3ae149353b5b2924f01
Elias <eeschmoelz@gmail.com>
2023-04-05 20:53:52 +0100

    feat: fetch operators data for operators page

--- app/(search+player)/player/[uuid]/operators/page.tsx
+++ app/(search+player)/player/[uuid]/operators/page.tsx
@@ -1,12 +1,21 @@
+import OperatorsGrid from "@/components/Operators/OperatorsGrid";
+import { fetchPlayer } from "@/lib/api/fetchPlayer";
+import styles from "./OperatorPage.module.scss";
+
 export const dynamic = 'force-static', dynamicParams = true;
 export function generateStaticParams() {
     return [];
 }
 
-export default function Operators() {
+export default async function Operators({ params }: { params: { uuid: string }}) {
+    const {data: { operators }} = await fetchPlayer(params.uuid, "uplay");
+    
+    // TODO: call notFound() if operators is null
+    if (!operators) return null;
+    
     return (
-        <div>
-            
+        <div className={styles.page}>
+            <OperatorsGrid data={operators}/>
         </div>
     )
 }
