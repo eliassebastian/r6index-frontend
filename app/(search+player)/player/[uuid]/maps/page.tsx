213a5a1fbb36fef76c0c3ea0bf848b3bb6a56f37
Elias <eeschmoelz@gmail.com>
2023-04-06 20:44:38 +0100

    feat: maps page list component

--- app/(search+player)/player/[uuid]/maps/page.tsx
+++ app/(search+player)/player/[uuid]/maps/page.tsx
@@ -1,12 +1,20 @@
+import { fetchPlayer } from "@/lib/api/fetchPlayer";
+import styles from "./MapsPage.module.scss";
+import MapsList from "@/components/Maps/MapsList";
+
 export const dynamic = 'force-static', dynamicParams = true;
 export function generateStaticParams() {
     return [];
 }
 
-export default function Maps() {
+export default async function Maps({ params }: { params: { uuid: string }}) {
+    const {data: { maps }} = await fetchPlayer(params.uuid, "uplay");
+    // TODO: call notFound() if maps is null
+    if (!maps) return null;
+
     return (
-        <div>
-            
+        <div className={styles.page}>
+            <MapsList data={maps}/>
         </div>
     )
 }
