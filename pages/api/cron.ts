434a7b4abcecb83e898330d862a06940f80adc78
Elias <eeschmoelz@gmail.com>
2023-05-03 16:47:38 +0100

    fix: log edge config error

--- pages/api/cron.ts
+++ pages/api/cron.ts
@@ -84,7 +84,7 @@ export default async function handler(request: NextRequest) {
         // check if edge config update was successful
         const result = await updateEdgeConfig.json();
         if (result.error) {
-            console.log("error updating edge config")
+            console.log("error updating edge config", result.error)
             return errorResponse(result.error);
         }
 
